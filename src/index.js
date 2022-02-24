6b3eeae64239af3bd9e531eadc9b9b4c055b266b
lserzhanl <57130298+lserzhanl@users.noreply.github.com>
2022-02-25 04:53:14 +0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  let existBracket = {};
+
+  for(let i = 0; i < bracketsConfig.length; i += 1) {
+    existBracket[bracketsConfig[i][0]] = bracketsConfig[i][1];
+  }
+
+  stack.push(str.charAt(0));
+
+  for(let i = 1; i < str.length; i += 1) {
+    if(str.charAt(i) === existBracket[stack[stack.length - 1]]) {
+      stack.pop();
+    } else {
+      stack.push(str.charAt(i)); 
+    }
+  }
+  if (stack.length > 0) {return false;}
+  return true;
 }
